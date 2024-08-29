50c19bc71f5b5a2d64cb41c1708f93480fc9611e
kenyounot123 <kenlu519@gmail.com>
2024-08-29 14:58:31 -0400

    Add book page creation in front end

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,7 +3,11 @@ import { GnovistarSidebar } from "./components/Sidebar";
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <GnovistarSidebar>
-        {children}
+      <div className="flex flex-1">
+        <div className="p-2 md:p-10 border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full overflow-y-auto">
+          {children}
+        </div>
+      </div>
     </GnovistarSidebar>
   );
 }
