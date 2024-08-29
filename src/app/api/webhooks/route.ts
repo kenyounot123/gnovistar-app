1869d1764eaf85f4dcffa80c4e8fc80b0c03e27e
Joshua-Alfred <joshua.jayapal@gmail.com>
2024-08-29 12:05:18 -0400

    webhook db record creation upon user.created

---
+++ src/app/api/webhooks/route.ts
@@ -0,0 +1,75 @@
+import { Webhook } from 'svix'
+import { headers } from 'next/headers'
+import { WebhookEvent } from '@clerk/nextjs/server'
+import { db } from '@/firebase'
+import { doc, setDoc } from 'firebase/firestore'
+
+export async function POST(req: Request) {
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+
+  if (!WEBHOOK_SECRET) {
+    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
+  } 
+
+  // Get the headers
+  const headerPayload = headers()
+  const svix_id = headerPayload.get('svix-id')
+  const svix_timestamp = headerPayload.get('svix-timestamp')
+  const svix_signature = headerPayload.get('svix-signature')
+
+  // If there are no headers, error out
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response('Error occured -- no svix headers', {
+      status: 400,
+    })
+  }
+
+  // Get the body
+  const payload = await req.json()
+  const body = JSON.stringify(payload)
+  console.log(body)
+
+  // Create a new Svix instance with your secret.
+  const wh = new Webhook(WEBHOOK_SECRET)
+
+  let evt: WebhookEvent
+
+  // Verify the payload with the headers
+  try {
+    evt = wh.verify(body, {
+      'svix-id': svix_id,
+      'svix-timestamp': svix_timestamp,
+      'svix-signature': svix_signature,
+    }) as WebhookEvent
+  } catch (err) {
+    console.error('Error verifying webhook:', err)
+    return new Response('Error occured', {
+      status: 400,
+    })
+  }
+
+  // Do something with the payload
+  // For this guide, you simply log the payload to the console
+  const { id } = evt.data
+  const userId: string = id || ""
+  console.log(userId)
+  const eventType = evt.type
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
+  if (eventType === 'user.created') {
+    try {
+      // Update Firestore with the new subscription field
+      const userRef = doc(db, 'users', userId);
+      await setDoc(userRef, { subscription: 'free' }, { merge: true });
+
+      console.log(`User ${id} subscription set to free`);
+    } catch (error) {
+      console.error('Error updating user subscription in Firestore:', error);
+      return new Response('Error occurred updating Firestore', {
+        status: 500,
+      });
+    }
+  }
+
+  return new Response('', { status: 200 })
+}
